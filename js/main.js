e231ddbb7512447c122d2ed786d5ee871b82ac39
KostyaPenyavskiy <kornelius9898@gmail.com>
2021-09-28 12:07:23 +0300

    initial

--- js/main.js
+++ js/main.js
@@ -20,3 +20,35 @@ const tick = setInterval(function() {
 
     --timer.seconds;
 }, 1000);
+
+// sender
+
+async function sender(name, phone, campaign_id) {
+    const response = await fetch('https://tracker.everad.com/conversion/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      mode: 'no-cors',
+      body: `name=${name}&phone=${phone}&campaign_id=${campaign_id}`
+    });
+    return response;
+  }
+
+// form submit
+
+const $form = document.querySelector('.form__order');
+
+$form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const data = new FormData(this);
+    const name = data.get('name');
+    const phone = data.get('phone');
+    const campaign_id = data.get('campaign_id');
+    const url = `subscribe.html?name=${name}&phone=${phone}&campaign_id=${campaign_id}`;
+
+    sender(name, phone, campaign_id);
+
+    window.open(url).focus();
+});
